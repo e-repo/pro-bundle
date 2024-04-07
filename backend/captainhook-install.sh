0123e1e6ed89c264e75b4092ef577ba4144ef9aa
Evgeny <postmail.evg@mail.ru>
2024-04-07 12:55:33 +0300

    Доработка capitainhook install после composer install

--- backend/captainhook-install.sh
+++ backend/captainhook-install.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env sh
 
+gitPath="../.git/hooks"
+
 if [ -n "$(composer show | grep 'captainhook/captainhook')" ];
 then
   vendor/bin/captainhook install -f --only-enabled --run-mode=docker --run-exec="docker-compose run --rm bb-cli" --run-path="vendor/bin/captainhook" > /dev/null 2>&1;
+
+  if [ ! -d "$gitPath" ]; then
+      echo "Директория $gitPath не существует."
+      exit 1
+  fi
+
+  for file in "$gitPath"/*; do
+      if grep -q -- '--configuration='"'backend\/captainhook.json'"'' "$file"; then
+          sed -i 's/--configuration='"'backend\/captainhook.json'"'/--configuration='"'captainhook.json'"'/g' "$file"
+
+          echo "Изменения в файле $file выполнены."
+      fi
+  done
 fi
