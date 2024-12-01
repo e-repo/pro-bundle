5deaa477b3dc76bc7c7153145518a11fb5f91af0
Evgeny <postmail.evg@mail.ru>
2024-12-01 19:26:31 +0300

    Подготовил CoreKit\UI\Http\Request\RequestArgumentResolver что-бы он мог принимать файлы и данные в одном запросе

--- backend/src/CoreKit/UI/Http/Request/RequestArgumentResolver.php
+++ backend/src/CoreKit/UI/Http/Request/RequestArgumentResolver.php
@@ -8,7 +8,10 @@ use CoreKit\UI\Http\Exception\ViolationException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use TypeError;
 
 final readonly class RequestArgumentResolver implements ValueResolverInterface
 {
@@ -37,10 +40,62 @@ final readonly class RequestArgumentResolver implements ValueResolverInterface
         );
 
         $dto = $this->payloadToDto($payload, $argumentType);
+        $dto = $this->addRequestFilesToDto($dto, $request);
 
         $this->checkViolation($dto);
 
         yield $dto;
+    }
+
+    private function addRequestFilesToDto(object $dto, Request $request): object
+    {
+        if ('form' !== $request->getContentTypeFormat()) {
+            return $dto;
+        }
+
+        foreach ($request->files->all() as $propertyName => $formData) {
+            if (false === property_exists($dto, $propertyName)) {
+                continue;
+            }
+
+            /**
+             * При заголовке multipart form data можно передавать
+             * вместе с файлом данные. Для этого в request выделяется
+             * свойство с именем 'data'
+             *
+             * @ToDo Для определения типа формируемого объекта данных
+             * идущих вместе с файлом возможно лучше использовать ArgumentMetadata,
+             * либо потискать другой способ. ArgumentMetadata::getType()
+             * ( $argumentType = $argument->getType() ) - не подойдет
+             */
+            //            if ($propertyName === 'data') {
+            //                $dto->{$propertyName} = $this->payloadToDto(
+            //                    payload: json_decode($formData, true, 512, JSON_THROW_ON_ERROR),
+            //                    argumentType: $argumentType
+            //                );
+            //
+            //                continue;
+            //            }
+
+            try {
+                $dto->{$propertyName} = $formData;
+            } catch (TypeError $exception) {
+                $constraintViolation = new ConstraintViolation(
+                    message: sprintf('Неверный тип данных у поля для передачи файлов \'%s\'', $propertyName),
+                    messageTemplate: '',
+                    parameters: [],
+                    root: null,
+                    propertyPath: null,
+                    invalidValue: null
+                );
+
+                throw new ViolationException(
+                    new ConstraintViolationList([$constraintViolation])
+                );
+            }
+        }
+
+        return $dto;
     }
 
     private function payloadToDto(array $payload, string $argumentType): mixed
